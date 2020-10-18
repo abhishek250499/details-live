1b3883825b9a4f01f0e6bc11ba071fdf59cde70b
Abhishek Singh <65482891+abhishek250499@users.noreply.github.com>
2020-10-18 17:36:14 +0530

    Add files via upload

--- assignment1/settings.py
+++ assignment1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,3 +138,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+django_heroku.settings(locals())
